f4c240c3380ecd988984a5e24969729db58cb118
Brendan Hellar and Zema Kebede <brendan@>
2019-04-22 15:17:05 -0700

    Add logic to determine what type of triangle is created by input numbers

--- triangleTracker.cs
+++ triangleTracker.cs
@@ -26,6 +26,28 @@ public class triangleTracker
     triangle.Three += int.Parse(Console.ReadLine());
 
     Console.WriteLine(triangle.One+","+triangle.Two+","+triangle.Three);
-  }
 
+    if (triangle.One == triangle.Two && triangle.Two == triangle.Three)
+    {
+      Console.WriteLine("Equilateral Triangle");
+    }
+    else if (triangle.One == triangle.Two && triangle.Three <(triangle.One+triangle.Two)||
+    triangle.One == triangle.Three && triangle.Two <(triangle.One+triangle.Three)||
+    triangle.Three == triangle.Two && triangle.One<(triangle.Three+triangle.Two))
+    {
+      Console.WriteLine("Isosceles Triangle");
+    }
+    else if (triangle.One != triangle.Two && triangle.Two != triangle.Three && triangle.One != triangle.Three &&
+       triangle.One < (triangle.Two + triangle.Three) &&
+       triangle.Two < (triangle.One + triangle.Three) &&
+       triangle.Three < (triangle.One + triangle.Two))
+    {
+      Console.WriteLine("Scalene Triangle");
+    }
+    else
+    {
+        Console.WriteLine("That's not a triangle");
+    }
+
+  }
 }
